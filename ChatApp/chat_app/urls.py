e5cb99d69fd8913a735029355d9132480343d72e
sunday <117924550+sunepa01@users.noreply.github.com>
2023-06-06 19:47:00 +0100

    Delete ChatApp directory

--- ChatApp/chat_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from chat_app import views
-
-urlpatterns = [
-    path('SignUp/', views.signUp),
-    path('Login/', views.login),
-    path('confirmCode/', views.confirmCode, name='confirm_code'),
-]
